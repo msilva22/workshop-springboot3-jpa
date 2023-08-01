7431fc43b665fff47bcfef64edfa21eb47f285d7
Moisés Silva <moises.ambiental@hotmail.com>
2023-08-01 14:49:30 -0300

    Payment, one-to-one association

--- src/main/java/com/exemplo1/course/config/TestConfig.java
+++ src/main/java/com/exemplo1/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.exemplo1.course.entities.Category;
 import com.exemplo1.course.entities.Order;
 import com.exemplo1.course.entities.OrderItem;
+import com.exemplo1.course.entities.Payment;
 import com.exemplo1.course.entities.Product;
 import com.exemplo1.course.entities.User;
 import com.exemplo1.course.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
